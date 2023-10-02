83913a8885a68d0b7a028214a1534cc8e65fc869
Gabriel <gabriel-euclides@hotmail.com>
2023-10-02 16:32:17 -0300

    criacao formulario

--- src/components/Projects/ProjectForm.js
+++ src/components/Projects/ProjectForm.js
@@ -1,24 +1,24 @@
-import Styles from './ProjectForm.module.css'
+import Input from '../form/Input'
+import Select from '../form/Select'
+import SubmitButton from '../form/SubmitButton'
+import styles from './ProjectForm.module.css'
 
-function ProjectForm() {
+function ProjectForm({btnText}) {
     return (
-        <form className={Styles.form}> 
-            <div>
-                <input type="Text" placeholder="Insira o nome do projeto" />
-            </div>
-            <div>
-                <input type="number" placeholder="Insira o orçamento total" />
-            </div>
-            <div>
-                <select name="category_id">
-                    <option disabled>
-                        Selecione a categoria
-                    </option>
-                </select>
-            </div>
-            <div>
-                <input type="submit" value="Criar Projeto"/>
-            </div>
+        <form className={styles.form}> 
+            <Input type="text" 
+            text="Nome do projeto" 
+            name="name" 
+            placeholder="Insira o nome do projeto"/>
+            
+            <Input type="number" 
+            text="Orçamento do projeto" 
+            name="budget" 
+            placeholder="Insira o orçamento total"/>
+            
+            <Select name="category_id"  text="Selecione a categoria"/>
+            
+            <SubmitButton text={btnText}/>
 
         </form>
     )
--- src/components/form/Input.js
+++ src/components/form/Input.js
@@ -1,11 +1,16 @@
 import Styles from './Input.module.css'
 
 
-function Input({type, text, name, placeholder, handleOnChange, value}){
-    return(
-        <div>
-            <label htmlFor=''></label>
-            <input type='text'/>
+function Input({ type, text, name, placeholder, handleOnChange, value }) {
+    return (
+        <div className={Styles.form_control}>
+            <label htmlFor={name}>{text}:</label>
+            <input type={type} 
+            name={name} 
+            id={name} 
+            placeholder={placeholder} 
+            onChange={handleOnChange} 
+            value={value} />
         </div>
     )
 }
